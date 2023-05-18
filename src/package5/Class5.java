495ed035d493e3eb56ed4d3169f153057f5f3ba2
KapkarPravin <pravin.kapkar26@gmail.com>
2023-05-18 09:41:02 +0530

    This is modified code for Narcissitic Number

--- src/package5/Class5.java
+++ src/package5/Class5.java
@@ -1,5 +1,34 @@
 package package5;
 
-public class Class5 {
+import java.util.Scanner;
 
-}
+public class Class5 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// This is code for Narcisstic Number
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number:");
+		int num=sc.nextInt();
+	
+         	int sum=0;
+			int num1=num;
+			int numOfDigits = String.valueOf(num).length();
+			while(num1>0) {
+				int digit = num1%10;
+				sum+=Math.pow(digit,numOfDigits);
+				num1=num1/10;
+			}
+			
+			if (sum==num) {
+			   System.out.println(num+ " is NarcissisticNumber");	
+			}
+			else {
+				   System.out.println(num+ " is not NarcissisticNumber");	
+
+			}
+
+	}
+	}
